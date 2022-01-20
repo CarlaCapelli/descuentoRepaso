6c270d8bfb32902373321246d1997d96e6190c23
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2022-01-20 20:15:07 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+//ejercicio descuento octubre
+/*let precio: number = Number(prompt("cual es el precio?"));
+let cant: number = Number(prompt("que cantidad va a llevar?"));
+let mes: string = String(prompt("en que mes se encuentra?"));
+let precioFinal: number = 0;
+let descuento: number = 0;
+if (mes === "octubre") {
+  descuento = precio * cant * 0.15;
+  precioFinal = precio * cant - descuento;
+
+  console.log("Por comprar en octubre el precio es $" + precioFinal);
+} else {
+  precioFinal = precio * cant;
+  console.log("Por no comprar en octubre el precio es $" + precioFinal);
+}
+
+//ejercicio- aplicar descuento
+
+let precio: number = Number(prompt("cual es el precio?"));
+let cant: number = Number(prompt("que cantidad va a llevar?"));
+
+
+let precioSinDesc: number = precio * cant ;
+let precioFinal:number=0;
+let descuento :number = precioSinDesc *0.1;
+if (precioSinDesc >1000){
+  precioFinal= precioSinDesc - descuento;
+  console.log("el precio con descuento por haber gastado mas de $1000 es de $" + precioFinal);
+
+}else{
+  precioFinal= precioSinDesc;
+  console.log("el precio sin descuento por haber gastado menos de $1000 es de $" + precioFinal);
+
+}
+*/
+
+//ejercicio- aumento de sueldo
+
+//let sueldo:number = Number(prompt("de cuanto es su sueldo?"));
